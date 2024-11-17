0de1697ca24e48b68b3b0b20a5dccd11d20f3cf6
林鹏 <1227839175@qq.com>
2024-11-17 17:36:22 +0800

    This is a Hansheng Intelligent IoT project.

--- src/wasm/source_codes/c++/math_tool/MathTool原版.cpp
+++ src/wasm/source_codes/c++/math_tool/MathTool原版.cpp
@@ -10,9 +10,9 @@ CreateDate: 2024-11-16 18:21:35 星期六
 /*
 生成对应的“.wasm”的命令：
 1、第一步：
-在“MathTool.cpp”文件所在的文件夹下执行命令“emcmdprompt”。
+在“MathTool.cpp”文件所在的文件夹下执行命令“emsdk_env.bat”。
 2、第二步：
-emcc MathTool.cpp --std=c++2a -Os -s WASM=1 -s SIDE_MODULE=1 -o MathTool.wasm
+emcc MathTool.cpp -std=c++23 -Os -s WASM=1 -s SIDE_MODULE=1 -o MathTool.wasm
 */
 
 #include <climits>
